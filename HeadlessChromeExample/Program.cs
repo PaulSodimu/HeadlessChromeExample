3d2d4fe6a1c8f5f2f2840d703e9e7db84d0ee7e3
Paul Sodimu <paul.sodimu@hotmail.co.uk>
2017-08-20 10:54:25 -0500

    Headless chrome example

--- HeadlessChromeExample/Program.cs
+++ HeadlessChromeExample/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System; 
 
 namespace HeadlessChromeExample
 {
@@ -10,6 +8,19 @@ namespace HeadlessChromeExample
   {
     static void Main(string[] args)
     {
+      //Create crome options with headles arg
+      ChromeOptions options = new ChromeOptions();
+      options.AddArgument("--headless");
+
+      var driver = new ChromeDriver(options);
+      driver.Url = "http://www.paulsodimu.co.uk";
+
+      //Take Screenshot 
+      Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+      ss.SaveAsFile($"{AppContext.BaseDirectory}\\Shot.png", ScreenshotImageFormat.Png);
+
+      Console.ReadKey();
+      driver.Quit();
     }
   }
 }
